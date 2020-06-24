bed1f37ec84808bf51863e009c53fb2e505dee8f
ian-1 <51008396ian-1@users.noreply.github.com>
2020-06-24 12:43:48 +0100

    improve add action window, with labels, entries and subit and destroy window submit button

--- views/action.py
+++ views/action.py
@@ -1,11 +1,28 @@
 
 from tkinter import Toplevel,\
+                    Label,\
+                    Entry,\
                     Button
 
 class Action:
     # view items used in add action window
 
+    def add_action(window, user, property, message):
+        user.add_action(property, message)
+        window.destroy()
+
     def add_window(window, user):
-        filewin = Toplevel(window)
-        button = Button(filewin, text='add_action', command=lambda: user.add_action('5 skdhfjksd', 'yweiou ufhuihf sdjk'))
+        add_action_window = Toplevel(window)
+
+        lable_property = Label(add_action_window, text='Property:')
+        lable_property.pack()
+        entry_property = Entry(add_action_window, bd = 5)
+        entry_property.pack()
+
+        lable_message = Label(add_action_window, text='Message:')
+        lable_message.pack()
+        entry_message = Entry(add_action_window, bd = 5)
+        entry_message.pack()
+
+        button = Button(add_action_window, text='ADD ACTION', command=lambda: Action.add_action(add_action_window, user, '5 skdhfjksd', 'yweiou ufhuihf sdjk'))
         button.pack()
