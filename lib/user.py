99f2d31d8c254f654a265a55f0bf6cec8a680510
ian-1 <51008396ian-1@users.noreply.github.com>
2020-06-24 11:24:37 +0100

    add add_action method to user class

--- lib/user.py
+++ lib/user.py
@@ -3,3 +3,6 @@ from lib.action import Action
 class User():
     def __init__(self):
         self.actions = []
+
+    def add_action(self, property, message):
+        self.actions.append(Action(property, message))
